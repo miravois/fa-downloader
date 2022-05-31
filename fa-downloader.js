fcf37c3716ad981d0c9cddd020f046a60a15875a
miravois <lacus.shen.ls@gmail.com>
2022-05-31 03:03:32 -0400

    [1.1.2] Added media rule to auto hide panel.

--- fa-downloader.js
+++ fa-downloader.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FA Downloader
 // @namespace    fa-downloader
-// @version      1.1.1
+// @version      1.1.2
 // @description  Download FA Media from sites including Twitter/Poipiku/Privatter/Lofter
 // @author       miravois
 // @license      MIT
@@ -137,6 +137,7 @@
     GM_addStyle('.fad__fileUploadImportDB {position:absolute !important;height:1px;width:1px;overflow:hidden;clip:rect(1px, 1px, 1px, 1px);}');
     GM_addStyle('.fad__fileUploadImportDB:focus+label,fad__fileUploadImportDB:focus-within+label {outline: thin dotted;}');
     GM_addStyle('.fad__poipiku__download {width:24px !important;height:24px !important;position:absolute;top:121px;left:0;padding:2.5px 50.5px;cursor:pointer;filter:opacity(0.5) drop-shadow(0 0 0 #0077DD) saturate(1000%);}');
+    GM_addStyle('@media screen and (max-width: 1800px) { .fad__panel { display:none; } }');
 
     const TwitterDownloadIconHTML = '<g xmlns="http://www.w3.org/2000/svg" style="color:#0077dd;"><g transform="rotate(-180 11.999625205993652,9.00012493133545)"><path d="M17.53 7.47l-5-5c-.293-.293-.768-.293-1.06 0l-5 5c-.294.293-.294.768 0 1.06s.767.294 1.06 0l3.72-3.72V15c0 .414.336.75.75.75s.75-.336.75-.75V4.81l3.72 3.72c.146.147.338.22.53.22s.384-.072.53-.22c.293-.293.293-.767 0-1.06z"/></g><g><path d="M19.708 21.944H4.292C3.028 21.944 2 20.916 2 19.652V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 .437.355.792.792.792h15.416c.437 0 .792-.355.792-.792V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 1.264-1.028 2.292-2.292 2.292z"/></g></g>'
     const TwitterMarkIconHTML = '<g xmlns="http://www.w3.org/2000/svg" style="color:#ff7722;"><g transform="rotate(0 11.999625205993652,9.00012493133545)"><path d="M17.53 7.47l-5-5c-.293-.293-.768-.293-1.06 0l-5 5c-.294.293-.294.768 0 1.06s.767.294 1.06 0l3.72-3.72V15c0 .414.336.75.75.75s.75-.336.75-.75V4.81l3.72 3.72c.146.147.338.22.53.22s.384-.072.53-.22c.293-.293.293-.767 0-1.06z"/></g><g><path d="M19.708 21.944H4.292C3.028 21.944 2 20.916 2 19.652V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 .437.355.792.792.792h15.416c.437 0 .792-.355.792-.792V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 1.264-1.028 2.292-2.292 2.292z"/></g></g>'
